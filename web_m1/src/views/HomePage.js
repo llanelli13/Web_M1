83bcf23221be72764a4790f6fea0d27bbef37ad7
llanelli13 <pierre.rey@efrei.net>
2023-11-02 14:28:45 +0100

    AJout page home + règlement + vidéo homepage

--- web_m1/src/views/HomePage.js
+++ web_m1/src/views/HomePage.js
@@ -1,10 +1,32 @@
-// src/views/HomePage.js
 import React from 'react';
+import './Style/HomePage.css'; 
 
-const HomePage = () => {
-    return (
-        <div>Home Page !</div>
-    );
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1 className="home-header">Bienvenue sur Pick'EM!</h1>
+
+      <div className="description-section">
+        <p className="description-text">
+          Pick'EM est un jeu de prédiction où vous essayez de deviner les résultats des matchs de League of Legends. Testez vos connaissances, défiez vos amis et tentez de remporter des récompenses incroyables!
+        </p>
+      </div>
+
+      <div className="demo-section">
+        <h2 className="demo-title">Comment jouer ?</h2>
+        <p className="demo-content">
+          Regardez la démo ci-dessous pour comprendre le fonctionnement du jeu. C'est simple, rapide et amusant!
+        </p>
+        {/* Vous pouvez intégrer une vidéo ou une animation de démo ici */}
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/1ZJrpFbQ_Lk?si=dy-EOpf1mnStQnkP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+      </div>
+
+      <button className="btn-start">Commencer à jouer!</button>
+      
+    </div>
+  );
 }
 
 export default HomePage;
--- web_m1/src/views/RulesPage.js
+++ web_m1/src/views/RulesPage.js
@@ -1,10 +1,48 @@
-// src/views/Rules.js
 import React from 'react';
+import './Style/RulesPage.css'; 
 
 const Rules = () => {
     return (
-        <div>Rules Page</div>
-    );
-}
+        <div className="regulation-page">
+          <h1 className="regulation-header">Règlement du Jeu Pick'EM</h1>
+          
+          <div className="regulation-section">
+            <h2 className="regulation-title">1. Objectif du jeu</h2>
+            <p className="regulation-content">
+              Le but de Pick'EM est de prédire les résultats des matchs de League of Legends. Les joueurs accumulent des points basés sur la précision de leurs prédictions.
+            </p>
+          </div>
+    
+          <div className="regulation-section">
+            <h2 className="regulation-title">2. Comment jouer</h2>
+            <p className="regulation-content">
+              Les joueurs doivent sélectionner les équipes qu'ils pensent qu'elles gagneront avant le début de chaque match. Une fois le match commencé, les sélections sont verrouillées et ne peuvent être modifiées.
+            </p>
+          </div>
+    
+          <div className="regulation-section">
+            <h2 className="regulation-title">3. Attribution des points</h2>
+            <p className="regulation-content">
+              Les points sont attribués en fonction de la précision des prédictions des joueurs. Une prédiction correcte rapporte un certain nombre de points, qui varie en fonction de la phase du tournoi.
+            </p>
+          </div>
+    
+          <div className="regulation-section">
+            <h2 className="regulation-title">4. Récompenses</h2>
+            <p className="regulation-content">
+              Les joueurs qui obtiennent le plus de points à la fin du tournoi recevront des récompenses. Les détails spécifiques des récompenses seront annoncés séparément.
+            </p>
+          </div>
+    
+          <div className="regulation-section">
+            <h2 className="regulation-title">5. Conditions générales</h2>
+            <p className="regulation-content">
+              En participant à Pick'EM, les joueurs acceptent de respecter les termes et conditions du jeu. Tout comportement inapproprié ou tentative de tricherie entraînera une disqualification.
+            </p>
+          </div>
+          
+        </div>
+      );
+    }
 
 export default Rules;
